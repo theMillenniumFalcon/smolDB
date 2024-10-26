99f314c9000889a2dafad82c189fb707eaa86de0
themillenniumfalcon <nishankpr2002@gmail.com>
2024-10-26 20:06:44 +0530

    added index command

--- shell.go
+++ shell.go
@@ -5,6 +5,7 @@ import (
 	"os"
 	"strings"
 
+	"github.com/themillenniumfalcon/smolDB/index"
 	"github.com/themillenniumfalcon/smolDB/log"
 )
 
@@ -29,8 +30,21 @@ func shell(dir string) error {
 	}
 }
 
-func execInput(input string) error {
+func execInput(input string) (err error) {
+	input = strings.TrimSuffix(input, "\n")
 	args := strings.Split(input, " ")
-	log.Info("%+v", args)
-	return nil
+
+	switch args[0] {
+	case "index":
+		files := index.I.ListKeys()
+		log.Success("found %d files in index:", len(files))
+		for _, f := range files {
+			log.Info(f)
+		}
+	default:
+		log.Warn("'%s' is not a valid command.", args[0])
+		log.Info("valid commands: index, lookup <key>, delete <key>, exit")
+	}
+
+	return err
 }
